fc168da273844d08cf4c67ccdab98d395f826af2
liyingdaseu <921752157@qq.com>
2014-02-20 00:50:21 +0800

    the AddContentModel is being constructed! It's very late today, but I am very happy because the website is being stronger!!!

--- Application/Home/Controller/AddContentController.class.php
+++ Application/Home/Controller/AddContentController.class.php
@@ -6,30 +6,34 @@ class AddContentController extends Controller{
 		$receiveInfo["loseId"]=I('post.loseId');
 		$receiveInfo["pickId"]=I('post.pickId');
 		$receiveInfo["addCount"]=I('post.addCount');
-		$receiveInfo["typeFilter"]=I('post.typeFilter');	//这是一个布尔数组，包含类型过滤的信息，元素有stationery、electronic、card、money、keys、others
+		$receiveInfo["typeFilter"]=I('post.typeFilter');//这是一个布尔数组，包含类型过滤的信息，元素有stationery、electronic、card、money、keys、others
 
-		for($i=0;$i<$receiveInfo["addCount"];$i++)
+		$addconMod=D('AddContent');	//实例化添加信息模型
+		$outputArray=$addconMod->AddContent($receiveInfo);
+
+		for($i=0; $i<count($outputArray); $i++)
 		{
 			
 			//用Modal代替以下内容
-
+			
 			
 
-			$md=D('AddContent');
-			//$zd=$md->where('loseid=5')->getField('thing_name');
 
 
 
-			$infoType="con-pick";
+
+		/*
 			$thing_name="钥匙";			
 			$place="图书馆";
-			$datetime="2013-12-21 23:10:00";
+			$datetime=$queryInfo;
 			$thing_describe="很精致，像是女生的";
 			$picture_url="Public/pictures/default2.jpg";
-			$content_id=16;
+			$content_id=16;	
+			$infoType="con-pick";
+		*/
 
 			//'
-
+		/*
 			$outputArray["infoType"]=$infoType;	//用于区别是丢失还是捡到 con-pick/con-lose
 			$outputArray["thing_name"]=$thing_name;	//物品的名称
 			$outputArray["place"]=$place;	//丢失/捡到地点
@@ -37,7 +41,9 @@ class AddContentController extends Controller{
 			$outputArray["thing_describe"]=$thing_describe;	//对物品的描述
 			$outputArray["picture_url"]=$picture_url;
 			$outputArray["content_id"]=$content_id;
-			$this->assign($outputArray);
+		*/
+
+			$this->assign($outputArray[$i]);
 
 			$returnInfo["content"]=$returnInfo["content"].$this->fetch("AddContent");
 
--- Application/Home/Model/AddContentModel.class.php
+++ Application/Home/Model/AddContentModel.class.php
@@ -4,9 +4,83 @@ use Think\Model;
 class AddContentModel extends Model {
 	private $losesTable;
 	private $picksTable;
+
+	public function __construct($modelName){
+		$this->$losesTable=M('Loses');
+		$this->$picksTable=M('picks');
+	//	parent::__construct($modelName);
+	}
+
 	public function addContent($receiveInfo){
+		if($receiveInfo["loseId"]>=0 && $receiveInfo["pickId"]>=0){	//所有信息的情况
+			//这里要从两个数据表中按时间顺序同时读取消息
+
+			//获取最后更新的时间
+			$lastTime=$this->$losesTable->query('select update_time from loses where if_find=false order by update_time desc limit 1');
+			if(count($lastTime)>0){	//考虑到表为空的情况
+				$lastTime=$lastTime[0]['update_time'];
+			}else{
+				$lastTime=0;
+			}
+			$lastTime2=$this->$losesTable->query('select update_time from picks where if_give_back=false order by update_time desc limit 1');
+			if(count($lastTime2)){
+				$lastTime2=$lastTime2[0]['update_time'];
+			}else{
+				$lastTime2=0;
+			}
+
+			//进行首次内容载入
+			if($lastTime < $lastTime2){
+				$lastTime=$lastTime2;
+
+				$selectTable=$this->$losesTable->query('select pick_id,thing_name,place,datetime,thing_describe,if_has_picture
+					from picks where if_give_back=false
+					order by update_time desc limit 1');
+				$outputArray[0]["infoType"]="con-pick";
+				$outputArray[0]["content_id"]=$selectTable[0]["pick_id"];
+				$outputArray[0]["thing_name"]=$selectTable[0]["thing_name"];
+				$outputArray[0]["place"]=$selectTable[0]["place"];
+				$outputArray[0]["datetime"]=$selectTable[0]["datetime"];
+				$outputArray[0]["thing_describe"]=$selectTable[0]["thing_describe"];
+
+				//$outputArray[0]["picture_url"]=$selectTable[0]["picture_url"];
+			}else{
+				$selectTable=$this->$losesTable->query('select lose_id,thing_name,place,datetime,thing_describe,if_has_picture
+					from loses where if_find=false
+					order by update_time desc limit 1');
+				$outputArray[0]["infoType"]="con-lose";
+				$outputArray[0]["content_id"]=$selectTable[0]["lose_id"];
+				$outputArray[0]["thing_name"]=$selectTable[0]["thing_name"];
+				$outputArray[0]["place"]=$selectTable[0]["place"];
+				$outputArray[0]["datetime"]=$selectTable[0]["datetime"];
+				$outputArray[0]["thing_describe"]=$selectTable[0]["thing_describe"];
+				
+			}
+
+			for($i=1; $i<$receiveInfo["addCount"]; $i++){
+
+			}
+
+			
 
 
 
+
+
+
+
+
+		}
+		elseif($receiveInfo["loseId"]<0 && $receiveInfo["pickId"]>=0){	//有人捡到
+
+
+
+		}
+		elseif($receiveInfo["loseId"]>=0 && $receiveInfo["pickId"]<0){	//有人弄丢
+
+
+		}
+
+		return $outputArray;
 	}
 }
