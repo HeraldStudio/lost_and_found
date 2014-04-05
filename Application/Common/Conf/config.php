74bd073bacc865a49b2f112b0f02d21df640e81f
liyingdaseu <921752157@qq.com>
2014-04-05 20:30:11 +0800

    ok

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 	'DB_HOST' => 'localhost', // 服务器地址
 	'DB_NAME' => 'lost_and_found', // 数据库名
 	'DB_USER' => 'root', // 用户名
-	'DB_PWD' => '', // 密码
+	'DB_PWD' => '1234', // 密码
 	'DB_PORT' => '3306', // 端口
 	'DB_PREFIX' => '', // 数据库表前缀
 	//'DB_DSN' => '', // 数据库连接DSN 用于PDO方式
